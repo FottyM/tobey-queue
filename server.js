139a564c922efa873c99586fdbec603274c97dd7
Fortunat Mutunda <fotimutunda@gmail.com>
2022-06-13 20:03:16 +0300

    fix: cleanup

--- server.js
+++ server.js
@@ -1,24 +1,32 @@
 const express = require('express');
-const client = require('./db-client');
-const {userQueue} = require('./queues');
+const client = require('./src/db-client');
+const {userQueue} = require('./src/queues');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 let count = 0;
 
+const createUsersTableQuery = `
+  CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL
+  );
+`;
+
 app.post('/users', async (req, res) => {
-	const user = {
-		name: 'John Doe' + count,
-		email: 'email',
-	};
-	count++;
-	const job = await userQueue.add(user, {attempts: 3, delay: 1_000});
-	res.json({user, jobId: job.id});
+  const user = {
+    name: 'John Doe' + count,
+    email: 'email',
+  };
+  count++;
+  const job = await userQueue.add(user, {attempts: 3, delay: 1_000});
+  res.json({user, jobId: job.id});
 });
 
-client.connect().then(() => {
-	// Client.query('Create table users (id serial primary key, name varchar(255), email varchar(255))').then(() => {});
-	app.listen(port, () => console.log(`http://localhost:${port} !`));
+client.connect(async () => {
+  await client.query(createUsersTableQuery);
+  app.listen(port, () => console.log(`http://localhost:${port} !`));
 });
 
--- worker.js
+++ worker.js
@@ -1,40 +1,40 @@
-const client = require('./db-client');
-const {userQueue} = require('./queues');
+const client = require('./src/db-client');
+const {userQueue} = require('./src/queues');
 
 const concurrency = 2;
 const delay = 1_000;
 
 const sleep = ms => new Promise(resolve => {
-	setTimeout(resolve, ms);
+  setTimeout(resolve, ms);
 });
 
 client.connect(err => {
-	if (err) {
-		console.error(err);
-		return;
-	}
+  if (err) {
+    console.error(err);
+    return;
+  }
 
-	userQueue.process(concurrency, async job => {
-		console.log(`Processing job ${job.id}`);
+  userQueue.process(concurrency, async job => {
+    console.log(`Processing job ${job.id}`);
 
-		const {name, email} = job.data;
+    const {name, email} = job.data;
 
-		await sleep(delay);
+    await sleep(delay);
 
-		await client.query(`
+    await client.query(`
       INSERT INTO 
         users (name, email) 
         values ($1, $2)`,
-		[name, email],
-		);
-	});
+    [name, email],
+    );
+  });
 
-	userQueue.on('error', err => {
-		console.error(err);
-	});
+  userQueue.on('error', err => {
+    console.error(err);
+  });
 
-	userQueue.on('completed', data => {
-		console.error(`Job ${JSON.stringify(data, null, 2)} completed`);
-	});
+  userQueue.on('completed', data => {
+    console.error(`Job ${JSON.stringify(data, null, 2)} completed`);
+  });
 });
 
